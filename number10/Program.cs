a5e8ccf166e5440f026036f8d08041d470869dc9
yuliya <yulya.belova_17@mail.ru>
2017-03-01 12:24:48 +0500

    dodelat' number 10

---
+++ number10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number10
+{
+    class Program
+    {
+        static int Perimetr (int osnovanieBig, int osnovanieSmal, int high)
+        {
+            int katet = (osnovanieBig - osnovanieSmal)/2;
+            int gipotinyza = (int)(Math.Sqrt(Math.Pow(katet, 2)+Math.Pow(high,2)));
+            int perimetr = osnovanieSmal + osnovanieBig + gipotinyza * 2;
+            return perimetr;
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine();
+                int osnovanieBig = 10, osnovanieSmal = 2, high = 4;
+                int resultOfMethod = Perimetr(osnovanieBig, osnovanieSmal, high);
+                int sumOfPerimetr = 0;
+            }
+            
+
+        }
+    }
+}

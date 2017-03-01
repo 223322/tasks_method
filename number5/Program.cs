da2412d0bc9c6fd2793e484eaab75233dac9a00f
yuliya <yulya.belova_17@mail.ru>
2017-03-01 11:43:44 +0500

    end of project

--- number5/Program.cs
+++ number5/Program.cs
@@ -8,9 +8,26 @@ namespace number5
 {
     class Program
     {
+        static double Vichislenie (double Chislitel, double Znamenatel) 
+        {
+            double result = (Chislitel + Math.Sin(Chislitel)) / (Math.Sin(Znamenatel) + Znamenatel);
+            return result;
+        }
         static void Main(string[] args)
         {
-            double y;
+            double peremennayaVChislitele = 2, peremennayaVZnamenatele = 5;
+            double resultOfMethod = Vichislenie(peremennayaVChislitele, peremennayaVZnamenatele);
+            double y = 0;
+            y = y + resultOfMethod;
+            peremennayaVChislitele = 6;
+            peremennayaVZnamenatele = 3;
+            resultOfMethod = Vichislenie(peremennayaVChislitele, peremennayaVZnamenatele);
+            y = y + resultOfMethod;
+            peremennayaVChislitele = 1;
+            peremennayaVZnamenatele = 4;
+            resultOfMethod = Vichislenie(peremennayaVChislitele, peremennayaVZnamenatele);
+            y = y + resultOfMethod;
+            Console.WriteLine(y);
         }
     }
 }
